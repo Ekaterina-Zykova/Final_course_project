6814b46a58240592291a3c12ae07433f4b1d811e
enzkva <enzkva@gmail.com>
2021-01-22 15:08:35 +0300

    fix tests

--- tests/test_bot.py
+++ tests/test_bot.py
@@ -24,9 +24,9 @@ async def test_help_command(client: TelegramClient):
         await conv.send_message("/help")
         resp = await conv.get_response()
         assert (
-                f"For add channel in channel list, write:\n    /add_channel @name"
-                f"\nFor delete channel from channel list, write:\n    /delete_channel @name"
-                f"\nFor show channel list, write:\n    /channel_list" in resp.raw_text
+            f"For add channel in channel list, write:\n    /add_channel @name"
+            f"\nFor delete channel from channel list, write:\n    /delete_channel @name"
+            f"\nFor show channel list, write:\n    /channel_list" in resp.raw_text
         )
 
 
@@ -35,8 +35,8 @@ async def test_add_channel_command(client: TelegramClient):
         await conv.send_message("/add_channel")
         resp1 = await conv.get_response()
         assert (
-                "For add channel in channel list, write:\n    /add_channel @name"
-                in resp1.raw_text
+            "For add channel in channel list, write:\n    /add_channel @name"
+            in resp1.raw_text
         )
 
         await conv.send_message("/add_channel @channel1")
@@ -53,15 +53,15 @@ async def test_delete_channel_command(client: TelegramClient):
         await conv.send_message("/delete_channel")
         resp1 = await conv.get_response()
         assert (
-                "For delete channel from channel list, write:\n    /delete_channel @name"
-                in resp1.raw_text
+            "For delete channel from channel list, write:\n    /delete_channel @name"
+            in resp1.raw_text
         )
 
         await conv.send_message("/delete_channel @channel1")
         resp2 = await conv.get_response()
         assert (
-                "Channel is deleted from channel list!\nYou can write next name of channel."
-                in resp2.raw_text
+            "Channel is deleted from channel list!\nYou can write next name of channel."
+            in resp2.raw_text
         )
 
         await conv.send_message("/delete_channel @channel1")
@@ -74,8 +74,8 @@ async def test_channel_list_command(client: TelegramClient):
         await conv.send_message("/channel_list")
         resp1 = await conv.get_response()
         assert (
-                "Channel list is empty 😔 \nFor add channel in channel list, write:\n    /add_channel @name"
-                in resp1.raw_text
+            "Channel list is empty 😔 \nFor add channel in channel list, write:\n    /add_channel @name"
+            in resp1.raw_text
         )
 
         await conv.send_message("/add_channel @channel1")
@@ -92,6 +92,24 @@ async def test_repost_messages_photo(client: TelegramClient):
         conv = client.conversation(config.bot)
         file_path = os.path.abspath(r"tests\cat.jpeg")
         await conv.send_message("It's a cat", file=file_path)
-        messages = await client.get_messages(config.channel)
-        assert "It's a cat" in messages[0].message
-        assert messages[0].media.photo is not None
+        message = await client.get_messages(config.channel)
+        assert "It's a cat" in message[0].message
+        assert message[0].media.photo is not None
+
+
+async def test_repost_messages_audio(client: TelegramClient):
+    async with client as client:
+        conv = client.conversation(config.bot)
+        file_path = os.path.abspath(r"tests\audio.mp3")
+        await conv.send_file(file_path)
+        message = await client.get_messages(config.channel)
+        assert message[0].media is not None
+
+
+async def test_repost_messages_video(client: TelegramClient):
+    async with client as client:
+        conv = client.conversation(config.bot)
+        file_path = os.path.abspath(r"tests\video.mp4")
+        await conv.send_file(file_path)
+        message = await client.get_messages(config.channel)
+        assert message[0].media is not None
